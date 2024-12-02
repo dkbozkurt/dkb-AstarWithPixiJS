06c46b789e2de7828931e28ff83880712084e89e
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-12-02 14:53:09 +0100

    Cell base

--- src/GameBase/World/Cell.js
+++ src/GameBase/World/Cell.js
@@ -1,20 +1,42 @@
 import * as PIXI from 'pixi.js'
 import GameBase from '../GameBase.js'
+import GridSystem from './GridSystem.js'
 
 export default class Cell {
 
-    constructor(x,y) {
+    constructor(x, y) {
         this.gameBase = new GameBase()
         this.sizes = this.gameBase.sizes
         this.resources = this.gameBase.resources
         this.scene = this.gameBase.scene
+        this.gridSystem = new GridSystem()
 
         this.Id = -1
+        this.isValid = true
 
-        this.setup(x,y)
+        this.blockedCells = []
+
+        this.colors = {}
+        this.colors.valid = 0x9290FF
+        this.colors.inValid = 0xFF000D
+        this.colors.start = 0xFFFFFF
+        this.colors.target = 0x000000
+        this.colors.onClosedList = 0x2513FF
+        this.colors.onOpenList = 0xB8B2FF
+
+        this.currentColor = this.colors.valid
+
+        this.isOnOpenList = false
+        this.isOnClosedList = false
+
+        this.fScore = 0
+        this.gScore = 0
+        this.hScore = 0
+
+        this.setup(x, y)
     }
 
-    setup(xCoord,yCoord) {
+    setup(xCoord, yCoord) {
         this.cellModel = new PIXI.Sprite(this.resources.items["cellTexture"])
         this.cellModel.scale.set(0.5, 0.5)
 
